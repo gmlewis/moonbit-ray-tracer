cbcd134f4f95ccc9bbc9f31f9adf746add69f5ed
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-12-06 13:09:32 -0500

    Generalize patterns
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

---
+++ run-all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -ex
+TARGET=js
+moon run --target ${TARGET} src/ch1/main.mbt
+moon run --target ${TARGET} src/ch2/main.mbt
+moon run --target ${TARGET} src/ch4/main.mbt
+moon run --target ${TARGET} src/ch5/main.mbt
+moon run --target ${TARGET} src/ch6/main.mbt
+moon run --target ${TARGET} src/ch7/main.mbt
+moon run --target ${TARGET} src/ch9/main.mbt
+moon run --target ${TARGET} src/ch10-stripes/main.mbt
