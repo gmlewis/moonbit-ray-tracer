4a9e839c592d86c4f6bb434c4efd5f8f8986e279
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-12-06 13:31:31 -0500

    Add src/ch10-gradients
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- run-all.sh
+++ run-all.sh
@@ -8,3 +8,4 @@ moon run --target ${TARGET} src/ch6/main.mbt
 moon run --target ${TARGET} src/ch7/main.mbt
 moon run --target ${TARGET} src/ch9/main.mbt
 moon run --target ${TARGET} src/ch10-stripes/main.mbt
+moon run --target ${TARGET} src/ch10-gradients/main.mbt
