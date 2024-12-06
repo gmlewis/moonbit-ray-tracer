aac28c2216e3ccc75841cea492918f9ab1cd9ac6
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-12-06 15:22:35 -0500

    Add checkers and gradient_checkers
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- run-all.sh
+++ run-all.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -ex
 TARGET=js
-moon run --target ${TARGET} src/ch1/main.mbt
-moon run --target ${TARGET} src/ch2/main.mbt
-moon run --target ${TARGET} src/ch4/main.mbt
-moon run --target ${TARGET} src/ch5/main.mbt
-moon run --target ${TARGET} src/ch6/main.mbt
-moon run --target ${TARGET} src/ch7/main.mbt
-moon run --target ${TARGET} src/ch9/main.mbt
-moon run --target ${TARGET} src/ch10-stripes/main.mbt
-moon run --target ${TARGET} src/ch10-gradients/main.mbt
-moon run --target ${TARGET} src/ch10-rings/main.mbt
-moon run --target ${TARGET} src/ch10-gradient-rings/main.mbt
+moon run --target ${TARGET} src/ch1
+moon run --target ${TARGET} src/ch2
+moon run --target ${TARGET} src/ch4
+moon run --target ${TARGET} src/ch5
+moon run --target ${TARGET} src/ch6
+moon run --target ${TARGET} src/ch7
+moon run --target ${TARGET} src/ch9
+moon run --target ${TARGET} src/ch10-stripes
+moon run --target ${TARGET} src/ch10-gradients
+moon run --target ${TARGET} src/ch10-rings
+moon run --target ${TARGET} src/ch10-gradient-rings
+moon run --target ${TARGET} src/ch10-checkers
+moon run --target ${TARGET} src/ch10-gradient-checkers
