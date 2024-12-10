3a1a7a4f89e0dcb2f485cf1752b827556585b508
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-12-10 09:14:09 -0500

    Add ch14
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- run-all.sh
+++ run-all.sh
@@ -1,15 +1,16 @@
 #!/bin/bash -ex
 TARGET=js
-moon run --target ${TARGET} src/ch1
-moon run --target ${TARGET} src/ch2
-moon run --target ${TARGET} src/ch4
-moon run --target ${TARGET} src/ch5
-moon run --target ${TARGET} src/ch6
-moon run --target ${TARGET} src/ch7
-moon run --target ${TARGET} src/ch9
+moon run --target ${TARGET} src/ch01
+moon run --target ${TARGET} src/ch02
+moon run --target ${TARGET} src/ch04
+moon run --target ${TARGET} src/ch05
+moon run --target ${TARGET} src/ch06
+moon run --target ${TARGET} src/ch07
+moon run --target ${TARGET} src/ch09
 moon run --target ${TARGET} src/ch10-stripes
 moon run --target ${TARGET} src/ch10-gradients
 moon run --target ${TARGET} src/ch10-rings
 moon run --target ${TARGET} src/ch10-gradient-rings
 moon run --target ${TARGET} src/ch10-checkers
 moon run --target ${TARGET} src/ch10-gradient-checkers
+moon run --target ${TARGET} src/ch14
