2d5f4021ca900cfbbaa1d75082b5bd464378aee0
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2025-12-25 09:56:41 -0500

    Implement texture mapping
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -ex
 moon update && moon install && rm -rf target
-moon add moonbitlang/x
-moon fmt && moon info
-moon test --target native
-moon test --target all
+moon add bob/toml
+moon add gmlewis/image
+moon add gmlewis/io
+moon add moonbitlang/async
+moon add TheWaWaR/clap
+./test-all.sh
