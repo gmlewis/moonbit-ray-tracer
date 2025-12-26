cce92bfac9657cddcfffd49bff4d8ca3932b8619
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2025-12-25 20:08:03 -0500

    Fix PBR demo
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- run-all.sh
+++ run-all.sh
@@ -16,6 +16,7 @@
 # moon run src/ch15
 
 # Scene examples using the render CLI
+moon run src/render -- -s examples/art-gallery.toml -o art-gallery.png --samples 4 --divide 1
 moon run src/render -- -s examples/checkers-scene.toml -o checkers-scene.png --samples 8 --divide 1
 moon run src/render -- -s examples/csg-demo.toml -o csg-demo.png --samples 4 --divide 1
 moon run src/render -- -s examples/environment-demo.toml -o environment-demo.png --samples 4 --divide 1
@@ -24,4 +25,3 @@ moon run src/render -- -s examples/patterns-showcase.toml -o patterns-showcase.p
 moon run src/render -- -s examples/pbr-demo.toml -o pbr-demo.png --samples 4 --divide 1
 moon run src/render -- -s examples/simple-sphere.toml -o simple-sphere.png --samples 4 --divide 1
 moon run src/render -- -s examples/texture-demo.toml -o texture-demo.png --samples 4 --divide 1
-moon run src/render -- -s examples/art-gallery.toml -o art-gallery.png --samples 4 --divide 1
